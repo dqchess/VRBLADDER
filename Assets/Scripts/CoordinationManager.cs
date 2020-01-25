0926eeb58498f404813e217b4aa109c729147a2f
Ayoub Khammassi <ayoub.khammassi1997@gmail.com>
2020-01-25 17:55:03 +0100

    Added bacteria and biofilm animation

---
+++ Assets/Scripts/CoordinationManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoordinationManager : MonoBehaviour
+{
+    public Animator catheter;
+    public Animator urineSea;
+    public Animator bacteria;
+    public Animator biofilm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
